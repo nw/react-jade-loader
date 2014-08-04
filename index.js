c8bae3eb9371440298ec5bdddcbdb94529719f8b
Nathan White <nw@nwhite.net>
2014-08-04 04:54:47 -0600

    added ability to write multiple components in a single file

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ var jade = require('react-jade')
   , loaderUtils = require("loader-utils");
 
 module.exports = function(source){
+  
+  var transform = "var React = require('react');\n";
+  
   this.cacheable && this.cacheable();
+  
   var filepath = loaderUtils.getRemainingRequest(this).replace(/^!/, "");
-  var transformed = jade.compile(source, {filename: filepath});
-  return 'var React = require(\'react\');\nmodule.exports= ' + transformed.toString();
+  var query = loaderUtils.parseQuery(this.query);
+  
+  if(query.split){
+    var chunks = source.split(/\n*?\/\/ react: (\w+)\s*\n/);
+    chunks.shift();
+    if(!chunks.length) return single();
+
+    for(var i=0; i < chunks.length-1; i += 2){
+      transform += "exports['" + chunks[i] + "'] = " + jade.compile(chunks[i+1], {filename: filepath}).toString() + ";\n";
+    }
+    return transform;
+  }
+  
+  return single();
+  
+  function single(){
+    return transform + "module.exports= " + jade.compile(source, {filename: filepath}).toString();
+  }
+  
 }
